9dd4fec07940b58dcdd1a6c48b48114585e823c7
Amine Najahi <anajahi@codeaurora.org>
2019-05-03 14:32:39 -0400

    drm/msm/sde: use first vblank event to trigger early wakeup
    When hardware vsync is enable use the first vblank event
    after power collapse to trigger early wakeup. This will
    enable all mdp and dsi clocks as part of the early wakeup,
    reducing the processing load on first frame and improving
    frame drop count.
    
    Bug: 120610745
    
    Change-Id: I5d57553ecde4b21dda60a5ec7fd96070c16149de
    Signed-off-by: Amine Najahi <anajahi@codeaurora.org>

--- drivers/gpu/drm/msm/sde/sde_encoder.h
+++ drivers/gpu/drm/msm/sde/sde_encoder.h
@@ -349,4 +349,12 @@ int sde_encoder_in_cont_splash(struct drm_encoder *enc);
  */
 int sde_encoder_get_ctlstart_timeout_state(struct drm_encoder *enc);
 
+#ifndef CONFIG_MACH_XIAOMI_F7B
+/**
+ * sde_encoder_trigger_early_wakeup - trigger early wake up
+ * @drm_enc:    Pointer to drm encoder structure
+ */
+void sde_encoder_trigger_early_wakeup(struct drm_encoder *drm_enc);
+#endif
+
 #endif /* __SDE_ENCODER_H__ */
