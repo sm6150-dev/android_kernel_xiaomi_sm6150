f60422897bdd39bc1f8374105e6f334a95e6df57
Pig <pig.priv@gmail.com>
2020-10-01 11:59:59 +0800

    ALSA: compress: Revert to 57c2bb95b76b203c6ff05ab7de5d29666c147b9c
    Change-Id: I236c6d7ad585bd506ba03c72dc65b7040fb7a23f

--- sound/core/compress_offload.c
+++ sound/core/compress_offload.c
@@ -578,7 +578,10 @@ snd_compr_set_params(struct snd_compr_stream *stream, unsigned long arg)
 		stream->metadata_set = false;
 		stream->next_track = false;
 
-		stream->runtime->state = SNDRV_PCM_STATE_SETUP;
+		if (stream->direction == SND_COMPRESS_PLAYBACK)
+			stream->runtime->state = SNDRV_PCM_STATE_SETUP;
+		else
+			stream->runtime->state = SNDRV_PCM_STATE_PREPARED;
 	} else {
 		return -EPERM;
 	}
@@ -695,17 +698,8 @@ static int snd_compr_start(struct snd_compr_stream *stream)
 {
 	int retval;
 
-	switch (stream->runtime->state) {
-	case SNDRV_PCM_STATE_SETUP:
-		if (stream->direction != SND_COMPRESS_CAPTURE)
-			return -EPERM;
-		break;
-	case SNDRV_PCM_STATE_PREPARED:
-		break;
-	default:
+	if (stream->runtime->state != SNDRV_PCM_STATE_PREPARED)
 		return -EPERM;
-	}
-
 	retval = stream->ops->trigger(stream, SNDRV_PCM_TRIGGER_START);
 	if (!retval)
 		stream->runtime->state = SNDRV_PCM_STATE_RUNNING;
@@ -716,20 +710,11 @@ static int snd_compr_stop(struct snd_compr_stream *stream)
 {
 	int retval;
 
-	switch (stream->runtime->state) {
-	case SNDRV_PCM_STATE_OPEN:
-	case SNDRV_PCM_STATE_SETUP:
-	case SNDRV_PCM_STATE_PREPARED:
+	if (stream->runtime->state == SNDRV_PCM_STATE_PREPARED ||
+			stream->runtime->state == SNDRV_PCM_STATE_SETUP)
 		return -EPERM;
-	default:
-		break;
-	}
-
 	retval = stream->ops->trigger(stream, SNDRV_PCM_TRIGGER_STOP);
 	if (!retval) {
-		/* clear flags and stop any drain wait */
-		stream->partial_drain = false;
-		stream->metadata_set = false;
 		stream->runtime->state = SNDRV_PCM_STATE_SETUP;
 		wake_up(&stream->runtime->sleep);
 		stream->runtime->total_bytes_available = 0;
@@ -786,20 +771,12 @@ static int snd_compr_drain(struct snd_compr_stream *stream)
 	int retval;
 
 	mutex_lock(&stream->device->lock);
-	switch (stream->runtime->state) {
-	case SNDRV_PCM_STATE_OPEN:
-	case SNDRV_PCM_STATE_SETUP:
-	case SNDRV_PCM_STATE_PREPARED:
-	case SNDRV_PCM_STATE_PAUSED:
+	if (stream->runtime->state == SNDRV_PCM_STATE_PREPARED ||
+			stream->runtime->state == SNDRV_PCM_STATE_SETUP) {
 		retval = -EPERM;
 		goto ret;
-	case SNDRV_PCM_STATE_XRUN:
-		retval = -EPIPE;
-		goto ret;
-	default:
-		break;
 	}
-
+	mutex_unlock(&stream->device->lock);
 	retval = stream->ops->trigger(stream, SND_COMPR_TRIGGER_DRAIN);
 	mutex_lock(&stream->device->lock);
 	if (!retval) {
@@ -821,10 +798,6 @@ static int snd_compr_next_track(struct snd_compr_stream *stream)
 	if (stream->runtime->state != SNDRV_PCM_STATE_RUNNING)
 		return -EPERM;
 
-	/* next track doesn't have any meaning for capture streams */
-	if (stream->direction == SND_COMPRESS_CAPTURE)
-		return -EPERM;
-
 	/* you can signal next track if this is intended to be a gapless stream
 	 * and current track metadata is set
 	 */
@@ -843,35 +816,20 @@ static int snd_compr_partial_drain(struct snd_compr_stream *stream)
 {
 	int retval;
 
-	switch (stream->runtime->state) {
-	case SNDRV_PCM_STATE_OPEN:
-	case SNDRV_PCM_STATE_SETUP:
-	case SNDRV_PCM_STATE_PREPARED:
-	case SNDRV_PCM_STATE_PAUSED:
-		retval = -EPERM;
-		goto ret;
-	case SNDRV_PCM_STATE_XRUN:
-		retval = -EPIPE;
-		goto ret;
-	default:
-		break;
-	}
-
-	/* partial drain doesn't have any meaning for capture streams */
-	if (stream->direction == SND_COMPRESS_CAPTURE)
+	mutex_lock(&stream->device->lock);
+	if (stream->runtime->state == SNDRV_PCM_STATE_PREPARED ||
+			stream->runtime->state == SNDRV_PCM_STATE_SETUP) {
+		mutex_unlock(&stream->device->lock);
 		return -EPERM;
-
+	}
+	mutex_unlock(&stream->device->lock);
 	/* stream can be drained only when next track has been signalled */
 	if (stream->next_track == false)
 		return -EPERM;
 
-	stream->partial_drain = true;
 	retval = stream->ops->trigger(stream, SND_COMPR_TRIGGER_PARTIAL_DRAIN);
 
 	stream->next_track = false;
-
-ret:
-	mutex_unlock(&stream->device->lock);
 	return retval;
 }
 
