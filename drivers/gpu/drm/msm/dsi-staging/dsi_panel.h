e904004c6100b765a4152144befb3a3feea6e33b
Pig <pig.priv@gmail.com>
2021-02-08 02:04:40 +0800

    drm: Rewrite fod handling based off Xiaomi's implementation
    Change-Id: I3948d6dbb72176da8a06788a65a4a91d98dd2170

--- drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
+++ drivers/gpu/drm/msm/dsi-staging/dsi_panel.h
@@ -256,6 +256,10 @@ struct dsi_panel {
 
 	struct brightness_alpha_pair *fod_dim_lut;
 	u32 fod_dim_lut_count;
+
+	u32 fod_ui_ready;
+	u32 hbm_ntfy_skip_flag;
+	bool fod_dimlayer_hbm_enabled;
 };
 
 static inline bool dsi_panel_ulps_feature_enabled(struct dsi_panel *panel)
